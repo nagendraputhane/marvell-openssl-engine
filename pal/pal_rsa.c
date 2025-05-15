4a14c4e583d0bbb8e7baab5c084f77669ac6e368
Venkata Ravichandra Mynidi <vmynidi@marvell.com>
2025-05-15 13:34:22 +0000

    fix rsa pal code build issue
    Signed-off-by: Venkata Ravichandra Mynidi <vmynidi@marvell.com>

--- pal/pal_rsa.c
+++ pal/pal_rsa.c
@@ -158,7 +158,7 @@ int pal_dpdk_rsa_priv_enc(pal_rsa_ctx_t *pal_ctx, int flen,
 		setup_non_crt_pub_op_xform(rsa_xform, pal_ctx);
 
 #if RTE_VERSION >= RTE_VERSION_NUM(24, 11, 0, 0)
- 	 if (padding == RSA_NO_PADDING)
+	if (padding == PAL_RSA_NO_PADDING)
     		rsa_xform->rsa.padding.type = RTE_CRYPTO_RSA_PADDING_NONE;
   	else
     		rsa_xform->rsa.padding.type = RTE_CRYPTO_RSA_PADDING_PKCS1_5;
@@ -251,7 +251,7 @@ int pal_dpdk_rsa_pub_dec(pal_rsa_ctx_t *pal_ctx, int flen,
 	setup_non_crt_pub_op_xform(rsa_xform, pal_ctx);
 
 #if RTE_VERSION >= RTE_VERSION_NUM(24, 11, 0, 0)
-  	if (padding == RSA_NO_PADDING)
+	if (padding == PAL_RSA_NO_PADDING)
     		rsa_xform->rsa.padding.type = RTE_CRYPTO_RSA_PADDING_NONE;
   	else
     		rsa_xform->rsa.padding.type = RTE_CRYPTO_RSA_PADDING_PKCS1_5;
@@ -335,7 +335,7 @@ int pal_dpdk_rsa_pub_enc(pal_rsa_ctx_t *pal_ctx, int flen,
 	setup_non_crt_pub_op_xform(rsa_xform, pal_ctx);
 
 #if RTE_VERSION >= RTE_VERSION_NUM(24, 11, 0, 0)
-  	if (padding == RSA_NO_PADDING)
+	if (padding == PAL_RSA_NO_PADDING)
     		rsa_xform->rsa.padding.type = RTE_CRYPTO_RSA_PADDING_NONE;
   	else
     		rsa_xform->rsa.padding.type = RTE_CRYPTO_RSA_PADDING_PKCS1_5;
@@ -412,7 +412,7 @@ int pal_dpdk_rsa_priv_dec(pal_rsa_ctx_t *pal_ctx, int flen,
 	setup_crt_priv_op_xform(rsa_xform, pal_ctx);
 
 #if RTE_VERSION >= RTE_VERSION_NUM(24, 11, 0, 0)
-  	if (padding == RSA_NO_PADDING)
+	if (padding == PAL_RSA_NO_PADDING)
     		rsa_xform->rsa.padding.type = RTE_CRYPTO_RSA_PADDING_NONE;
   	else
     		rsa_xform->rsa.padding.type = RTE_CRYPTO_RSA_PADDING_PKCS1_5;
