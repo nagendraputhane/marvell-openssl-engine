c21c80fd58927475fae2fe0358bafb1bfd7c05b6
Garvit Varshney <gvarshney@marvell.com>
2025-07-15 13:23:43 +0530

    lc: optimize AES CBC
    Use non-in-place operation to avoid memcpy
    between input and output buffers with
    allocating total memory once per request.
    
    Type: feature
    JIRA: https://essjira.marvell.com/browse/IPBUSW-71959
    
    Signed-off-by: Garvit Varshney <gvarshney@marvell.com>
    Change-Id: I8fc9c31a46a4beeddc6b07effc923056c5faf429
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/toolkits/openssl-engine-dpdk/+/157767
    Reviewed-by: Venkata Ravichandra Mynidi <vmynidi@marvell.com>
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>

--- pal/liquid_crypto/lc_cbc.c
+++ pal/liquid_crypto/lc_cbc.c
@@ -13,21 +13,31 @@
 
 extern int cpt_num_cipher_pipeline_requests_in_flight;
 
+#define FREE_RESOURCES(start, end) \
+	do { \
+		for (int i = (start); i < (end); i++) { \
+			if (total_memory[i] != NULL) \
+				pal_free(total_memory[i]); \
+		} \
+	} while (0)
+
 int pal_aes_cbc_cipher(pal_cbc_ctx_t *pal_ctx, unsigned char *out, const unsigned char *in,
 						size_t inl, unsigned char *iv, int enc, int sym_queue, void *wctx)
 {
 	uint8_t dev_id = glb_params.dev_id;
 	sym_queue = glb_params.qp_id;
 	struct dao_lc_res deq_op_ptr[PAL_NUM_DEQUEUED_OPS];
-	struct dao_lc_buf *in_buf[MAX_PIPE_JOBS] = {NULL};
+	struct dao_lc_buf *in_buf[MAX_PIPE_JOBS], *out_buf[MAX_PIPE_JOBS];
 	unsigned char saved_iv[PAL_AES_CBC_IV_LENGTH];
 	const unsigned char *next_iv;
-	struct dao_lc_sym_op *enq_op_ptr[MAX_PIPE_JOBS] = {NULL};
-	pal_cry_op_status_t *status_ptr[MAX_PIPE_JOBS] = {NULL}, *new_st_ptr[PAL_NUM_DEQUEUED_OPS];
+	struct dao_lc_sym_op enq_op_ptr[MAX_PIPE_JOBS];
+	pal_cry_op_status_t *status_ptr[MAX_PIPE_JOBS], *new_st_ptr[PAL_NUM_DEQUEUED_OPS];
 	async_pipe_job_t pip_jobs[MAX_PIPE_JOBS];
 	uint8_t pip_jb_qsz = 0;
-	int ret = 1, numpipes, numalloc = 0, k, i;
+	int ret = 1, numpipes, k, i, chain_buf_cnt;
 	volatile uint16_t num_enqueued_ops, num_dequeued_ops;
+	void *total_memory[MAX_PIPE_JOBS];
+	uintptr_t base;
 
 	numpipes = pal_ctx->numpipes;
 	/* Bydefault number of pipe is one */
@@ -39,7 +49,7 @@ int pal_aes_cbc_cipher(pal_cbc_ctx_t *pal_ctx, unsigned char *out, const unsigne
 		pal_ctx->input_len = &inl;
 	}
 
-	memset(&deq_op_ptr, 0, sizeof(deq_op_ptr));
+	chain_buf_cnt = (inl + LIQUID_CRYPTO_BUF_SZ_MAX - 1) / LIQUID_CRYPTO_BUF_SZ_MAX;
 
 	for (i = 0; i < numpipes; i++)
 	{
@@ -47,7 +57,7 @@ int pal_aes_cbc_cipher(pal_cbc_ctx_t *pal_ctx, unsigned char *out, const unsigne
 		{
 			engine_log(ENG_LOG_ERR, "Invalid input length\n");
 			ret = -1;
-			goto free_resources;
+			goto exit;
 		}
 		// For decrytion, save the last iv_len bytes of ciphertext as next IV.
 		if (!enc)
@@ -56,46 +66,59 @@ int pal_aes_cbc_cipher(pal_cbc_ctx_t *pal_ctx, unsigned char *out, const unsigne
 						pal_ctx->input_len[i] - PAL_AES_CBC_IV_LENGTH;
 			memcpy(saved_iv, next_iv, PAL_AES_CBC_IV_LENGTH);
 		}
-		enq_op_ptr[i] = (struct dao_lc_sym_op *)(pal_malloc(sizeof(struct dao_lc_sym_op)));
-		if (unlikely(enq_op_ptr[i] == NULL))
+
+		/**
+		 * Allocates a memory block for each operation in the total_memory.
+		 *
+		 * The allocated memory size is calculated as the sum of:
+		 * - Twice the size of `struct dao_lc_buf` multiplied by `chain_buf_cnt` (for input and output buffers).
+		 * - The size of a `pal_cry_op_status_t` (operation status).
+		 * - Three times the cache line size (`RTE_CACHE_LINE_SIZE`) multiplied by `chain_buf_cnt` (for alignment or padding).
+		 *   (One for aligning `in_buf`, one for `out_buf`, and one for `status_ptr`)
+		 *
+		 * This ensures that all necessary structures and buffers for a cryptographic operation are allocated contiguously,
+		 * potentially improving cache performance and simplifying memory management.
+		 *
+		 */
+		total_memory[i] = (void *)pal_malloc(
+			(sizeof(struct dao_lc_buf) * 2 * chain_buf_cnt) +
+			sizeof(pal_cry_op_status_t) + (3 * RTE_CACHE_LINE_SIZE * chain_buf_cnt));
+		if (unlikely(total_memory[i] == NULL))
 		{
 			engine_log(ENG_LOG_ERR, "Not enough crypto operations available\n");
 			ret = -1;
-			numalloc = i;
-			goto free_resources;
+			FREE_RESOURCES(0, i);
+			goto exit;
 		}
-		status_ptr[i] = (pal_cry_op_status_t *)(pal_malloc(sizeof(pal_cry_op_status_t)));
-		if (unlikely(status_ptr[i] == NULL))
-		{
-			engine_log(ENG_LOG_ERR, "Not enough crypto operation status available\n");
-			ret = -1;
-			numalloc = i;
-			goto free_resources;
-		}
+
+		/*
+		* This ensures that all structures are optimally aligned for cache efficiency.
+		*/
+		base = (uintptr_t)total_memory[i];
+		in_buf[i] = (struct dao_lc_buf *)RTE_PTR_ALIGN((void *)base, RTE_CACHE_LINE_SIZE);
+		out_buf[i] = (struct dao_lc_buf *)RTE_PTR_ALIGN((void *)(in_buf[i] + chain_buf_cnt), RTE_CACHE_LINE_SIZE);
+		status_ptr[i] = (pal_cry_op_status_t *)RTE_PTR_ALIGN((void *)(out_buf[i] + chain_buf_cnt), RTE_CACHE_LINE_SIZE);
 		status_ptr[i]->is_complete = 0;
 		status_ptr[i]->is_successful = 0;
 		status_ptr[i]->numpipes = numpipes;
 		status_ptr[i]->wctx_p = wctx;
 
 		/* Perform crypto operation */
-		enq_op_ptr[i]->op_cookie = (uint64_t)status_ptr[i];
-		enq_op_ptr[i]->sess_id = pal_ctx->event.sess_event.sess_id;
+		enq_op_ptr[i].op_cookie = (uint64_t)status_ptr[i];
+		enq_op_ptr[i].sess_id = pal_ctx->event.sess_event.sess_id;
 
-		enq_op_ptr[i]->encrypt = !!enc;
-		in_buf[i] = (struct dao_lc_buf *)(pal_malloc(sizeof(struct dao_lc_buf)));
-		if (unlikely(in_buf[i] == NULL))
-		{
-			engine_log(ENG_LOG_ERR, "Failed to allocate memory for input buffer\n");
-			ret = -1;
-			numalloc = i;
-			goto free_resources;
-		}
+		enq_op_ptr[i].encrypt = !!enc;
 
 		if (likely(pal_ctx->input_len[i] <= LIQUID_CRYPTO_BUF_SZ_MAX))
 		{
 			in_buf[i]->data = pal_ctx->input_buf[i];
 			in_buf[i]->total_len = pal_ctx->input_len[i];
 			in_buf[i]->frag_len = pal_ctx->input_len[i];
+			out_buf[i]->data = pal_ctx->output_buf[i];
+			out_buf[i]->total_len = pal_ctx->input_len[i];
+			out_buf[i]->frag_len = pal_ctx->input_len[i];
+			in_buf[i]->next = NULL;
+			out_buf[i]->next = NULL;
 		}
 		else
 		{
@@ -105,6 +128,7 @@ int pal_aes_cbc_cipher(pal_cbc_ctx_t *pal_ctx, unsigned char *out, const unsigne
 			seg_buf->total_len = pal_ctx->input_len[i];
 			seg_buf->next = NULL;
 
+			int j = 1;
 			while (remaining > 0)
 			{
 				long int seg = remaining > LIQUID_CRYPTO_BUF_SZ_MAX ? LIQUID_CRYPTO_BUF_SZ_MAX : remaining;
@@ -114,16 +138,8 @@ int pal_aes_cbc_cipher(pal_cbc_ctx_t *pal_ctx, unsigned char *out, const unsigne
 				remaining -= seg;
 				if (remaining > 0)
 				{
-					seg_buf->next = pal_malloc(sizeof(struct dao_lc_buf));
-					if (unlikely(seg_buf->next == NULL))
-					{
-						engine_log(ENG_LOG_ERR, "Failed to allocate memory for input buffer struct\n");
-						ret = -1;
-						numalloc = i;
-						goto free_resources;
-					}
+					seg_buf->next = (struct dao_lc_buf *)RTE_PTR_ALIGN((void *)(in_buf[i] + j++), RTE_CACHE_LINE_SIZE);
 					seg_buf = seg_buf->next;
-					memset(seg_buf, 0, sizeof(struct dao_lc_buf));
 				}
 				else
 				{
@@ -131,10 +147,17 @@ int pal_aes_cbc_cipher(pal_cbc_ctx_t *pal_ctx, unsigned char *out, const unsigne
 				}
 			}
 		}
-		enq_op_ptr[i]->in_buffer = in_buf[i];
-		enq_op_ptr[i]->cipher_offset = 0;
-		enq_op_ptr[i]->cipher_len = pal_ctx->input_len[i];
-		enq_op_ptr[i]->cipher_iv = iv;
+		enq_op_ptr[i].in_buffer = in_buf[i];
+		enq_op_ptr[i].out_buffer = out_buf[i];
+		enq_op_ptr[i].cipher_offset = 0;
+		enq_op_ptr[i].cipher_len = pal_ctx->input_len[i];
+		enq_op_ptr[i].cipher_iv = iv;
+		enq_op_ptr[i].auth_iv = NULL;
+		enq_op_ptr[i].auth_len = 0;
+		enq_op_ptr[i].auth_offset = 0;
+		enq_op_ptr[i].aad = NULL;
+		enq_op_ptr[i].aad_len = 0;
+		enq_op_ptr[i].digest = NULL;
 	}
 
 	/* Enqueue this crypto operation in the crypto device */
@@ -144,18 +167,19 @@ int pal_aes_cbc_cipher(pal_cbc_ctx_t *pal_ctx, unsigned char *out, const unsigne
 		num_enqueued_ops +=
 			dao_liquid_crypto_sym_enqueue_burst(
 				dev_id, sym_queue,
-				enq_op_ptr[num_enqueued_ops],
+				&enq_op_ptr[num_enqueued_ops],
 				numpipes - num_enqueued_ops);
 	}
 	if (unlikely(num_enqueued_ops < numpipes))
 	{
 		engine_log(ENG_LOG_ERR, "Enqueue failed - too many attempts\n");
-		numalloc = numpipes;
 		ret = -1;
-		goto free_resources;
+		FREE_RESOURCES(num_enqueued_ops, numpipes);
+		for (k = 0; k < num_enqueued_ops; k++)
+			status_ptr[k]->numpipes = num_enqueued_ops;
 	}
 
-	CPT_ATOMIC_INC_N(cpt_num_cipher_pipeline_requests_in_flight, numpipes);
+	CPT_ATOMIC_INC_N(cpt_num_cipher_pipeline_requests_in_flight, num_enqueued_ops);
 	/* Handle asynchronous callback */
 	if (wctx && pal_ctx->async_cb)
 		pal_ctx->async_cb(NULL, NULL, 0, NULL, NULL, ASYNC_JOB_PAUSE);
@@ -197,57 +221,24 @@ int pal_aes_cbc_cipher(pal_cbc_ctx_t *pal_ctx, unsigned char *out, const unsigne
 	}
 
 	/* Decrement pipeline requests in flight */
-	CPT_ATOMIC_DEC_N(cpt_num_cipher_pipeline_requests_in_flight, numpipes);
+	CPT_ATOMIC_DEC_N(cpt_num_cipher_pipeline_requests_in_flight, num_enqueued_ops);
 
-	for (i = 0; i < numpipes; i++)
+	for (i = 0; i < num_enqueued_ops; i++)
 	{
 		/* Returns -1 if any of the pipe in the async job is failed. */
 		if (unlikely(!status_ptr[i]->is_successful))
 			ret = -1;
-		memcpy(pal_ctx->output_buf[i], enq_op_ptr[i]->in_buffer->data, pal_ctx->input_len[i]);
 		// For encryption, copy last 16 bytes of ciphertext to IV
 		if (enc)
 			next_iv = (pal_ctx->output_buf[i] + pal_ctx->input_len[i] - PAL_AES_CBC_IV_LENGTH);
 		else
 			next_iv = saved_iv;
 		memcpy(iv, next_iv, PAL_AES_CBC_IV_LENGTH);
-		pal_free(enq_op_ptr[i]);
-		// Free all segments in the input buffer chain
-		struct dao_lc_buf *seg = in_buf[i], *next_seg;
-		while (seg) {
-			next_seg = seg->next;
-			pal_free(seg);
-			seg = next_seg;
-		}
-		pal_free(status_ptr[i]);
+		pal_free(total_memory[i]);
 	}
 
-free_resources:
-	if (unlikely(ret < 0))
-	{
-		for (i = 0; i < numalloc; i++)
-		{
-			if (enq_op_ptr[i])
-			{
-				pal_free(enq_op_ptr[i]);
-				enq_op_ptr[i] = NULL;
-			}
-			if (status_ptr[i])
-			{
-				pal_free(status_ptr[i]);
-				status_ptr[i] = NULL;
-			}
-			// Free all segments in the input buffer chain
-			struct dao_lc_buf *seg = in_buf[i], *next_seg;
-			while (seg)
-			{
-				next_seg = seg->next;
-				pal_free(seg);
-				seg = next_seg;
-			}
-			in_buf[i] = NULL;
-		}
-	}
+exit:
+
 	pal_ctx->output_buf = NULL;
 	pal_ctx->input_buf = NULL;
 	pal_ctx->input_len = NULL;
